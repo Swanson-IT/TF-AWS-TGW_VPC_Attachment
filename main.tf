e4fa3581abb49924e81fd6d6203b1fbdad38dd77
swansoj2 <swansoj2@uc.edu>
2024-11-13 10:59:17 -0500

    removed output from main.tf

--- main.tf
+++ main.tf
@@ -8,9 +8,3 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "gateway_attachment" {
     Name = var.name
   }
 }
-
-# Output for the attachment ID
-output "gateway_attachment_id" {
-  description = "ID of the Gateway VPC Attachment"
-  value       = aws_ec2_transit_gateway_vpc_attachment.gateway_attachment.id
-}
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "gateway_attachment_id" {
   description = "ID of the Gateway VPC Attachment"
-  value       = aws_vpc_attachment.gateway_attachment.id
+  value       = aws_ec2_transit_gateway_vpc_attachment.gateway_attachment.id
 }
